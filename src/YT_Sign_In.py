2d597a7125f891c241d26651db32ab3fe38656b8
krisli210 <31546987+krisli210@users.noreply.github.com>
2019-10-20 13:04:53 -0500

    s

---
+++ src/YT_Sign_In.py
@@ -0,0 +1,9 @@
+from webbot import Browser
+
+web = Browser()
+
+web.go_to('google.com')
+web.type('youtube tv')
+web.click('YouTube TV Official Site | Watch Live Football Games‎')
+web.click('SIGN IN')
+
